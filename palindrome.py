b3dbe65295500d03dcefb24bb2745ce89fb7b2f0
biruktesfu <it.biruk.tesfaye@gmail.com>
2021-03-05 17:14:01 +0300

    leetcode initial commit

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+listor = []
+listlast = []
+def palindrome(x):
+    x = str(x)
+    for i in x:
+        listor.append(i)
+    j = 1
+    while (j < len(listor) + 1):
+        listlast.append(listor[len(listor) - j])
+        j+=1
+    if listor == listlast:
+        return True
+    else:
+        return False
+
+
+
+print(palindrome(123454321))
+
+
+
+
